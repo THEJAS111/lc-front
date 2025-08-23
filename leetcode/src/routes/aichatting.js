c4a62818b3145b00d5c23e170092fc826fdef676
THEJAS111 <tejasshetty189@gmail.com>
2025-08-23 17:00:26 +0530

    Delete leetcode directory

--- leetcode/src/routes/aichatting.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const aiRouter=express.Router();
-const usermiddleware=require('../middleware/usermiddleware');
-const solvedoubt=require('../controllers/solvedoubt')
-
-
-aiRouter.post("/chat",usermiddleware,solvedoubt);
-
-module.exports=aiRouter;
