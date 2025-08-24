fb9c08cc05971f00f09fd6c04d4236b1358a7c65
THEJAS111 <tejasshetty189@gmail.com>
2025-08-24 16:43:17 +0530

    Update axiosclient.js

--- lc-f/frontend/src/utils/axiosclient.js
+++ lc-f/frontend/src/utils/axiosclient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_BASE_URL, // for Vite
+  baseURL: import.meta.env.BACKEND_BASE_URL, // for Vite
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
